6efce3e5780927a1df6a411860dc1d1ea87fb9ec
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2021-05-18 11:40:05 +0530

    Update cowin.py

--- cowin/cowin.py
+++ cowin/cowin.py
@@ -2,10 +2,10 @@ import requests
 
 
 def getStates():
-    # to get the
-    # district_id = input("Enter the district ID: ")
-    # date = input("Enter the date (dd-mm-yyyy): ")
-    sessison = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=266&date=20-05-2021"
+    # to get the covid vaccine session details
+    district_id = input("Enter the district ID: ")
+    date = input("Enter the date (dd-mm-yyyy): ")
+    sessison = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id="+district_id+"&date="+date
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
     my_json = requests.get(sessison, headers=headers).json()
@@ -14,6 +14,7 @@ def getStates():
     print(f'Session Details')
     for i in my_json['sessions']:
         c = c + 1
+        print('-'*100)
         print('Place: ', c)
         print("center id: ", i['center_id'], "\nCenter Name:", i['name'], '\nAdrresss: ', i['address'], '\nDistrict: ',
               i['district_name'], '\nState: ', i['state_name'], '\nPincode: ', i['pincode'])
