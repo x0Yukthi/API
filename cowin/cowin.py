a2af4e392ccaf2c66896da23682ac519b9737b13
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2021-05-18 11:33:52 +0530

    Update cowin.py

--- cowin/cowin.py
+++ cowin/cowin.py
@@ -10,7 +10,18 @@ def getStates():
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
     my_json = requests.get(sessison, headers=headers).json()
 
-    print(my_json['sessions'])
+    c = 0
+    print(f'Session Details')
+    for i in my_json['sessions']:
+        c = c + 1
+        print('Place: ', c)
+        print("center id: ", i['center_id'], "\nCenter Name:", i['name'], '\nAdrresss: ', i['address'], '\nDistrict: ',
+              i['district_name'], '\nState: ', i['state_name'], '\nPincode: ', i['pincode'])
+        print('Date: ', i['date'], '\nTotal available capacity: ', i['available_capacity'],
+              '\nAvailable Capacity for first dose: ', i['available_capacity_dose1'],
+              '\nAvailable Capacity for second dose: ', i['available_capacity_dose2'])
+        print('Fee: ', i['fee'], '\nAge limit: ', i['min_age_limit'], '\nVaccine name: ', i['vaccine'])
+        print('Available slots', i['slots'])
 
 
 getStates()
